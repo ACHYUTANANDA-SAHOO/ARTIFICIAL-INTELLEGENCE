cad4934a9cc0469ca771c430a6fdde067bcea755
ACHYUTANANDA-SAHOO <113246116+ACHYUTANANDA-SAHOO@users.noreply.github.com>
2022-09-14 10:20:00 +0530

    Update 12_tic_tac_toe.py

--- DAY 3 LAB/12_tic_tac_toe.py
+++ DAY 3 LAB/12_tic_tac_toe.py
@@ -1,116 +1,58 @@
-# Tic-Tac-Toe Program using
-# random number in Python
+def sum(a,b,c):
+    return a+b+c
+def printBoard(xState , zState):
 
-# importing all necessary libraries
-import numpy as np
-import random
-from time import sleep
+    zero =  'X' if xState[0] else('@' if zState[0] else 0 )
+    one =  'X' if xState[1] else('@' if zState[1] else 1 )
+    two =  'X' if xState[2] else('@' if zState[2] else 2 )
+    three =  'X' if xState[3] else('@' if zState[3] else 3 )
+    four =  'X' if xState[4] else('@' if zState[4] else 4 )
+    five =  'X' if xState[5] else('@' if zState[5] else 5 )
+    six =  'X' if xState[6] else('@' if zState[6] else 6 )
+    seven =  'X' if xState[7] else('@' if zState[7] else 7 )
+    eight =  'X' if xState[8] else('@' if zState[8] else 8 )
 
-# Creates an empty board
-def create_board():
- return(np.array([[0, 0, 0],
-     [0, 0, 0],
-     [0, 0, 0]]))
+    print(f" {zero} | {one} | {two} ")
+    print(f"---|---|---")
+    print(f" {three} | {four} | {five} ")
+    print(f"---|---|---")
+    print(f" {six} | {seven} | {eight} ")
 
-# Check for empty places on board
-def possibilities(board):
- l = []
- 
- for i in range(len(board)):
-  for j in range(len(board)):
-   
-   if board[i][j] == 0:
-    l.append((i, j))
- return(l)
+def checkWin(xState,zState) : 
+    wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    for win in wins :
+        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3) :
+            print("X won the game")
+            return 1
+        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3) :
+            print("O won the game.")
+            return 0
+    return -1
 
-# Select a random place for the player
-def random_place(board, player):
- selection = possibilities(board)
- current_loc = random.choice(selection)
- board[current_loc] = player
- return(board)
+if __name__ == "__main__" : 
 
-# Checks whether the player has three
-# of their marks in a horizontal row
-def row_win(board, player):
- for x in range(len(board)):
-  win = True
-  
-  for y in range(len(board)):
-   if board[x, y] != player:
-    win = False
-    continue
-    
-  if win == True:
-   return(win)
- return(win)
+    total_turns = 9
 
-# Checks whether the player has three
-# of their marks in a vertical row
-def col_win(board, player):
- for x in range(len(board)):
-  win = True
-  
-  for y in range(len(board)):
-   if board[y][x] != player:
-    win = False
-    continue
-    
-  if win == True:
-   return(win)
- return(win)
+    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    turn = 1 # 1 for X and 0 for O
+    print("Welcome to TIC-TAC-TOE")
+    while(True) : 
+        printBoard(xState, zState)
+        if(turn == 1):
+            print("X's Chance")
+            value = int(input("Please enter a value : "))
+            xState[value] = 1
+        else :
+            print("O's Chance")
+            value = int(input("Please enter a value : "))
+            zState[value] = 1
 
-# Checks whether the player has three
-# of their marks in a diagonal row
-def diag_win(board, player):
- win = True
- y = 0
- for x in range(len(board)):
-  if board[x, x] != player:
-   win = False
- if win:
-  return win
- win = True
- if win:
-  for x in range(len(board)):
-   y = len(board) - 1 - x
-   if board[x, y] != player:
-    win = False
- return win
+        total_turns = total_turns - 1
 
-# Evaluates whether there is
-# a winner or a tie
-def evaluate(board):
- winner = 0
- 
- for player in [1, 2]:
-  if (row_win(board, player) or
-   col_win(board,player) or
-   diag_win(board,player)):
-    
-   winner = player
-   
- if np.all(board != 0) and winner == 0:
-  winner = -1
- return winner
-
-# Main function to start the game
-def play_game():
- board, winner, counter = create_board(), 0, 1
- print(board)
- sleep(2)
- 
- while winner == 0:
-  for player in [1, 2]:
-   board = random_place(board, player)
-   print("Board after " + str(counter) + " move")
-   print(board)
-   sleep(2)
-   counter += 1
-   winner = evaluate(board)
-   if winner != 0:
-    break
- return(winner)
-
-# Driver Code
-print("Winner is: " + str(play_game()))
+        if(checkWin(xState, zState) != -1 or total_turns == 0 ):
+            print("GAME OVER")
+            printBoard(xState, zState)
+            break
+        turn = 1 -  turn 
+        
